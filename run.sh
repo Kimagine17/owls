c531a48793a2e0b2d03db8eb98b748e7474746e5
Kimberly McKinnon <kimberlite17@gmail.com>
2025-03-15 14:13:46 -0600

    Adding missing files

---
+++ run.sh
@@ -0,0 +1,12 @@
+# python detect_owls.py --weights runs/train/exp8/weights/best.pt --source ~/Desktop/baby_fresh_hatched.mkv  --view-img --vid-stride=100 --nosave  --device="mps"
+# python detect_owls.py --weights runs/train/exp8/weights/best.pt --source 'https://www.youtube.com/watch?v=u_4FJ4M7gGE'  --view-img --vid-stride=10 --nosave --device="mps"
+
+# python detect_owls.py --weights runs/train/exp8/weights/best.pt --source 'https://www.youtube.com/watch?v=Rl1-8XN45AM'  --view-img --vid-stride=10 --nosave --device="mps"
+# python detect_owls.py --weights runs/train/exp8/weights/best.pt --source ~/Desktop/owlstest_babies.mkv  --view-img --vid-stride=100 --nosave 
+# python detect_owls.py --weights runs/train/exp8/weights/best.pt --source 0 --view-img --vid-stride=5 --nosave --device="mps"
+python detect_owls.py  --source 0 --view-img --nosave --device="mps"
+
+# python detect_owls.py --weights runs/train/exp8/weights/best.pt --source 'https://www.youtube.com/watch?v=UOoxkddnpG4'  --view-img --vid-stride=10 --nosave --device="mps"
+# python detect_owls.py --weights runs/train/exp8/weights/best.pt --source 'https://www.youtube.com/watch?v=6f-D2AjeEi0'  --view-img --vid-stride=10 --nosave --device="mps"
+# python detect_owls.py  --weights runs/train/exp8/weights/best.pt --source 0  --view-img  --nosave --device="mps"
+
